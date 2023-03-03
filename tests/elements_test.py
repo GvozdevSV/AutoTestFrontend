4d4c380c73afcbdc146e9d528614cf47f0eebaa9
GvozdevSV <agasfen82@yandex.ru>
2023-03-03 11:21:39 +0400

    add allure

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,15 +1,29 @@
 import time
 
-from pages.elements_page import TextBoxPage
+import allure
+
+from pages.elements_page import TextBoxPage, CheckBoxPage
+
+@allure.suite("Elements")
 class TestElements:
+    @allure.feature("TextBox")
     class TestTextBox:
+        @allure.title("Check TextBox")
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            assert full_name == output_name, "the full name does not match"
-            assert email == output_email, "the email does not match"
-            assert current_address == output_cur_addr, "the current address does not match"
-            assert permanent_address == output_per_addr, "the permanent address does not match"
+            assert full_name == output_name, "ФИО не совпадает"
+            assert email == output_email, "электронная почта не совпадает"
+            assert current_address == output_cur_addr, "current address не совпадает"
+            assert permanent_address == output_per_addr, "permanent address не совпадает"
 
+    class TestCheckBox:
+        @allure.title("Check CheckBox")
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            time.sleep(4)
