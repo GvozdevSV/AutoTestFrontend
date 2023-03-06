d5c2e8ecd591007c233b853453422b59229c2e1d
GvozdevSV <agasfen82@yandex.ru>
2023-03-06 14:51:47 +0400

    add class WebTablesPage

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -4,7 +4,8 @@ import self as self
 from selenium.webdriver.common.by import By
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
+    WebTablesPageLocators
 from pages.base_page import BasePage
 
 class TextBoxPage(BasePage):
@@ -77,4 +78,27 @@ class RadioButtonPage(BasePage):
         self.element_is_visible(choices[choice]).click()
 
     def get_output_result(self):
-        return  self.element_is_present(self.locators.OUTPUT_RESULT).text
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
+
+class WebTablesPage(BasePage):
+    locators = WebTablesPageLocators()
+
+    def add_new_person(self):
+        count = 1
+        while count != 0:
+            person_info = next(generated_person())
+            first_name = person_info.first_name
+            last_name = person_info.last_name
+            email = person_info.email
+            age = person_info.age
+            salary = person_info.salary
+            department = person_info.department
+            self.element_is_visible(self.locators.ADD_RECORD_BUTTON).click()
+            self.element_is_visible(self.locators.FIRST_NAME_INPUT).send_keys(first_name)
+            self.element_is_visible(self.locators.LAST_NAME_INPUT).send_keys(last_name)
+            self.element_is_visible(self.locators.EMAIL_INPUT).send_keys(email)
+            self.element_is_visible(self.locators.AGE_INPUT).send_keys(age)
+            self.element_is_visible(self.locators.SALARY_INPUT).send_keys(salary)
+            self.element_is_visible(self.locators.DEPARTMENT_INPUT).send_keys(department)
+            count -= 1
+            return first_name, last_name, email, age, salary, department
