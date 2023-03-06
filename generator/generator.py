04de3580eb415c39e69044707aebf12adec5f8c9
GvozdevSV <agasfen82@yandex.ru>
2023-03-06 14:50:15 +0400

    add attrubut to genepated_persona

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -6,6 +8,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + "" + faker_ru.last_name() + "" + faker_ru.middle_name(),
+        first_name=faker_ru.first_name(),
+        last_name=faker_ru.last_name(),
+        age=random.randint(14,90),
+        salary=random.randint(20000,100000),
+        department=faker_ru.job(),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
