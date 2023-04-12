55143257c68cc0268fac566d1fb3b9b79da3b956
GvozdevSV <agasfen82@yandex.ru>
2023-04-12 07:08:53 +0400

    add allure

--- tests/form_test.py
+++ tests/form_test.py
@@ -1,10 +1,14 @@
 import time
 
+import allure
+
 from pages.form_page import FormPage
 
-
+@allure.suite('Form')
 class TestForm:
+    @allure.feature('Test form')
     class TestFormPage:
+        @allure.title('Checking the field form')
         def test_form(self, driver):
             form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
             form_page.open()
