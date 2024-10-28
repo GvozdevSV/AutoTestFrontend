797937b0f0abb990d1e7b53416d68094c73740ca
sgvozdev <agasfen82@yandex.ru>
2024-10-28 17:45:38 +0400

    update selenium

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService, Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
@@ -15,7 +15,11 @@ CI = os.getenv('CI')
 @pytest.fixture(scope='function')
 def driver():
     if CI == "False":
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        options = Options()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        driver = webdriver.Chrome(service=ChromeService(), options=options)
         driver.maximize_window()
     else:
         options = Options()
@@ -23,6 +27,6 @@ def driver():
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
-        driver = webdriver.Chrome(service=Service(), options=options)
+        driver = webdriver.Chrome(service=ChromeService(), options=options)
     yield driver
     driver.quit()
